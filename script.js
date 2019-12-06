fae81c21b71365af9111225605d128ee6015c758
Elena Angelova <eleangelo0717@gmail.com>
2019-12-06 17:19:44 +0700

    Считаем дни

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ alert('Javascript подключен')
 console.log ('jkgkuvyjh')
 
 function daysdoNY () {
-    todayData = new Date()
-    todayDay = todayData.getDate()
-    currentDate = new Date(2019, 12, 31)
-    currentDay = currentDate.getDate()
-    dateDifference = currentDate.getDate() - todayData.getDate()
-    console.log(dateDifference)
-    return dateDifference
+     year = '2020'
+     month = 01
+     day = 01
+    todayData = new Date();
+    nextDate=new Date([year,month,day].join(',')); 
+    msPerDay = 24*60*60*1000; 
+    nextDate1 = Math.floor((nextDate.getTime()-todayData.getTime())/msPerDay); 
+   
+
+    console.log(nextDate1)
+
+   return nextDate1
 }
 
-dayPlace.innerHTML=daysdoNY()
+   dayPlace.innerHTML=daysdoNY()
