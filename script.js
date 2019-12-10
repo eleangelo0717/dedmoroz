97ad51249ef8e6d8bc639fb5447a7d562a22d1df
Elena Angelova <eleangelo0717@gmail.com>
2019-12-10 22:41:27 +0700

    готово

--- script.js
+++ script.js
@@ -1,49 +1,58 @@
 
-day = 34
 
 let dayNum = document.getElementById("day_num")
 let dayDay = document.getElementById("day_day")
 let dayPlace = document.getElementById("day_place")
-alert('Javascript подключен')
 
 
-function daysdoNY () {
-     year = '2020'
-     month = 01
-     day = 01
-    todayData = new Date();
-    nextDate=new Date([year,month,day].join(',')); 
-    msPerDay = 24*60*60*1000; 
-    nextDate1 = Math.floor((nextDate.getTime()-todayData.getTime())/msPerDay); 
-    console.log(nextDate1)
-
+function daysdoNY() {
+   year = '2020'
+   month = '01'
+   day = '11'
+   todayData = new Date()
+   nextDate = new Date([year, month, day].join(','))
+   msPerDay = 24 * 60 * 60 * 1000
+   nextDate1 = Math.floor((nextDate.getTime() - todayData.getTime()) / msPerDay)
+   console.log(nextDate1)
    return nextDate1
 }
 
-   dayNum.innerHTML=daysdoNY()
+let days = daysdoNY()
+dayNum.innerHTML = days
 
- /*if (nextDate1=21){
-   dayDay.innerHTML='день'
-    console.log('день')
+let n = days % 10
+let n1 = days % 100
+let ickl = n1>10 && n1<15
+
+if (n == 1 && !ickl) {
+   dayDay.innerHTML = 'день'
+   dayPlace.innerHTML = 'остался'
+   console.log(n)
+}
+
+if (n > 1 && n < 5 && !ickl) {
+   dayDay.innerHTML = 'дня'
+   console.log(n)
+}
+
+
+/*let a = nextDate1
+ switch (a) {
+    case 21:
+     console.log(nextDate1 ')
+     dayDay.innerHTML='день'
+     dayPlace.innerHTML = 'остался'
+     break
+     case 22:
+     console.log(nextDate1 )
+     dayDay.innerHTML='дня'
+     break
+     case 25:
+     console.log(nextDate1)
+     dayDay.innerHTML='дней'
+     break
+  default:
+  console.log(nextDate1)
+  dayDay.innerHTML='/////'
+
  }*/
- 
- let a = nextDate1
-  switch (a) {
-     case 21:
-      console.log(nextDate1) 
-      dayDay.innerHTML='день'
-      dayPlace.innerHTML = 'остался'
-      break
-      case 22:
-      console.log(nextDate1 ) 
-      dayDay.innerHTML='дня'
-      break
-      case 25:
-      console.log(nextDate1) 
-      dayDay.innerHTML='дней'
-      break
-   default:
-   console.log(nextDate1)
-   dayDay.innerHTML='/////'
-
-  }
